b198bba994cb543bb695257cb6d9726b7c55080b
Relz <relz0071@gmail.com>
2017-09-26 16:57:40 +0000

    Табы изменены на пробелы.

--- Lab_0/src/main/java/ru/relz/javacore2017/HelloJava.java
+++ Lab_0/src/main/java/ru/relz/javacore2017/HelloJava.java
@@ -4,19 +4,18 @@ import java.io.File;
 import java.util.Arrays;
 
 class HelloJava {
-	public static void main(String[] args) {
-		String jrePath = System.getProperty("java.home");
-		String jdkPath = (new File(jrePath)).getParentFile().getAbsolutePath();
-		System.out.print("Hello, Java from ");
-		Arrays.stream(args).forEach(arg -> System.out.print(String.format("%s ", arg)));
-		System.out.println(
-			String.format("%s %s %s %s",
-				System.getProperty("os.name"),
-				System.getProperty("os.version"),
-				System.getProperty("java.version"),
-				jdkPath
-			)
-		);
-	}
+    public static void main(String[] args) {
+        String jrePath = System.getProperty("java.home");
+        String jdkPath = (new File(jrePath)).getParentFile().getAbsolutePath();
+        System.out.print("Hello, Java from ");
+        Arrays.stream(args).forEach(arg -> System.out.print(String.format("%s ", arg)));
+        System.out.println(
+                String.format("%s %s %s %s",
+                        System.getProperty("os.name"),
+                        System.getProperty("os.version"),
+                        System.getProperty("java.version"),
+                        jdkPath
+                )
+        );
+    }
 }
-
--- Lab_0/src/main/java/ru/relz/javacore2017/TopWords.java
+++ Lab_0/src/main/java/ru/relz/javacore2017/TopWords.java
@@ -10,68 +10,67 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 class TopWords {
-	public static void main(String[] args) {
-		if (args.length < 1) {
-			printlnError("Input file not specified");
-		}
-		String inputFileName = args[0];
-		forEachFileWord(inputFileName, new FileReading());
-	}
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            printlnError("Input file not specified");
+        }
+        String inputFileName = args[0];
+        forEachFileWord(inputFileName, new FileReading());
+    }
 
-	private static void printlnError(String msg) {
-		System.out.println(String.format("Error: %s", msg));
-		System.exit(1);
-	}
-	
-	private static void forEachFileWord(String fileName, FileReadingInterface callback) {
-		try {
-			Scanner scanner = new Scanner(new  File(fileName));
-			while (scanner.hasNext()) {
-				callback.onWordRead(scanner.next());
-			}
-			callback.onFileReadingFinished();
-		} catch (FileNotFoundException e) {
-			printlnError("Input file " + fileName + " not found");
-		}
-	}
+    private static void printlnError(String msg) {
+        System.out.println(String.format("Error: %s", msg));
+        System.exit(1);
+    }
 
-	interface FileReadingInterface {
-		void onWordRead(String word);
-		void onFileReadingFinished();
-	}
+    private static void forEachFileWord(String fileName, FileReadingInterface callback) {
+        try {
+            Scanner scanner = new Scanner(new  File(fileName));
+            while (scanner.hasNext()) {
+                callback.onWordRead(scanner.next());
+            }
+            callback.onFileReadingFinished();
+        } catch (FileNotFoundException e) {
+            printlnError("Input file " + fileName + " not found");
+        }
+    }
 
-	private static class FileReading implements FileReadingInterface {
-		HashMap<String, Integer> wordFrequenceMap = new HashMap<String, Integer>();
-		List<HashSet<String>> frequenceWordsList = new ArrayList<HashSet<String>>();
+    interface FileReadingInterface {
+        void onWordRead(String word);
+        void onFileReadingFinished();
+    }
 
-		@Override
-		public void onWordRead(String word) {
-			int oldWordFrequence = 0;
-			int newWordFrequence = 1;
-			if (wordFrequenceMap.containsKey(word)) {
-				oldWordFrequence = wordFrequenceMap.get(word);
-				newWordFrequence = oldWordFrequence + 1;
-			}
-			wordFrequenceMap.put(word, newWordFrequence);
-			if (oldWordFrequence != 0) {
-				frequenceWordsList.get(oldWordFrequence - 1).remove(word);
-			}
-			if (frequenceWordsList.size() < newWordFrequence) {
-				frequenceWordsList.add(new HashSet<String>());
-			}
-			frequenceWordsList.get(newWordFrequence - 1).add(word);
-		}
-	
-		@Override
-		public void onFileReadingFinished() {
-			ListIterator<HashSet<String>> listIterator = frequenceWordsList.listIterator(frequenceWordsList.size());
-			while (listIterator.hasPrevious()) {
-				HashSet<String> wordsSet = listIterator.previous();
-				for (String word: wordsSet) {
-					System.out.println(word);
-				}
-			}
-		}
-	}
+    private static class FileReading implements FileReadingInterface {
+        HashMap<String, Integer> wordFrequenceMap = new HashMap<String, Integer>();
+        List<HashSet<String>> frequenceWordsList = new ArrayList<HashSet<String>>();
+
+        @Override
+        public void onWordRead(String word) {
+            int oldWordFrequence = 0;
+            int newWordFrequence = 1;
+            if (wordFrequenceMap.containsKey(word)) {
+                oldWordFrequence = wordFrequenceMap.get(word);
+                newWordFrequence = oldWordFrequence + 1;
+            }
+            wordFrequenceMap.put(word, newWordFrequence);
+            if (oldWordFrequence != 0) {
+                frequenceWordsList.get(oldWordFrequence - 1).remove(word);
+            }
+            if (frequenceWordsList.size() < newWordFrequence) {
+                frequenceWordsList.add(new HashSet<String>());
+            }
+            frequenceWordsList.get(newWordFrequence - 1).add(word);
+        }
+
+        @Override
+        public void onFileReadingFinished() {
+            ListIterator<HashSet<String>> listIterator = frequenceWordsList.listIterator(frequenceWordsList.size());
+            while (listIterator.hasPrevious()) {
+                HashSet<String> wordsSet = listIterator.previous();
+                for (String word: wordsSet) {
+                    System.out.println(word);
+                }
+            }
+        }
+    }
 }
-
