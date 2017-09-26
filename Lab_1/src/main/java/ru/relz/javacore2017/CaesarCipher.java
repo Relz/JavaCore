6bb2b9448d997d41bc0b851d0d7034a1fe225e55
Relz <relz0071@gmail.com>
2017-09-26 17:02:52 +0000

    Исправлены ошибки, табы изменены на пробелы.

--- Lab_1/src/main/java/ru/relz/javacore2017/CaesarCipher.java
+++ Lab_1/src/main/java/ru/relz/javacore2017/CaesarCipher.java
@@ -3,41 +3,46 @@ package ru.relz.javacore2017;
 import java.lang.StringBuilder;
 
 class CaesarCipher {
-	private static int englishAlphabetSize = 26;
-	private static int firstLetterCode = (int) 'a';
-	private static int lastLetterCode = (int) 'z';
-	private static String encryptOption = "-e";
-	private static String decryptOption = "-d";
+    private static final int ENGLISH_ALPHABET_SIZE = 26;
+    private static final int FIRST_LETTER_CODE = (int) 'a';
+    private static final int LAST_LETTER_CODE = (int) 'z';
+    private static final String ENCRYPTION_OPTION = "-e";
+    private static final String DECRYPTION_OPTION = "-d";
 
-	public static void main(String[] args) {
-		if (args.length < 3) {
-			printlnError("CaesarCipher <encrypt|decrypt> <shift count> <input string>");
-		}
-		if (args[0].equals(encryptOption) && args[0].equals(decryptOption)) {
-			printlnError("Invalid option " + args[0] + ". Type " + encryptOption + " for encrypt or " + decryptOption + " for decrypt");
-		}
-		boolean encrypt = args[0].equals(encryptOption);
-		int shiftCount = Integer.parseInt(args[1]);
-		if (!encrypt) {
-			shiftCount = -shiftCount;
-		}
-		StringBuilder inputStringBuilder = new StringBuilder(args[2]);
-		for (int i = 0; i < inputStringBuilder.length(); ++i) {
-			int charPos = inputStringBuilder.codePointAt(i);
-			int newCharPos = charPos + shiftCount;
-			if (newCharPos < firstLetterCode) {
-				newCharPos += englishAlphabetSize;
-			} else if (newCharPos > lastLetterCode) {
-				newCharPos -= englishAlphabetSize;
-			}
-			inputStringBuilder.setCharAt(i, (char) (newCharPos));
-		}
-		System.out.println(inputStringBuilder);
-	}
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            printlnError("CaesarCipher <encrypt|decrypt> <shift count> <input string>");
+        }
+        if (!args[0].equals(ENCRYPTION_OPTION) && !args[0].equals(DECRYPTION_OPTION)) {
+            printlnError("Invalid option " + args[0] + ". Type " + ENCRYPTION_OPTION + " for encrypt or " + DECRYPTION_OPTION + " for decrypt");
+        }
+        boolean encrypt = args[0].equals(ENCRYPTION_OPTION);
+        int shiftCount = 0;
+        try {
+            shiftCount = Integer.parseInt(args[1]);
+        } catch(NumberFormatException ex) {
+            printlnError("Invalid argument <shift count>, number expected");
+        }
+        if (!encrypt) {
+            shiftCount = -shiftCount;
+        }
+        StringBuilder inputStringBuilder = new StringBuilder(args[2]);
+        for (int i = 0; i < inputStringBuilder.length(); ++i) {
+            int charPos = inputStringBuilder.codePointAt(i);
+            int newCharPos = charPos + shiftCount;
+            while (newCharPos < FIRST_LETTER_CODE) {
+                newCharPos += ENGLISH_ALPHABET_SIZE;
+            }
+            while (newCharPos > LAST_LETTER_CODE) {
+                newCharPos -= ENGLISH_ALPHABET_SIZE;
+            }
+            inputStringBuilder.setCharAt(i, (char) (newCharPos));
+        }
+        System.out.println(inputStringBuilder);
+    }
 
-	private static void printlnError(String msg) {
-		System.out.println(String.format("Error: %s", msg));
-		System.exit(1);
-	}
+    private static void printlnError(String msg) {
+        System.out.println(String.format("Error: %s", msg));
+        System.exit(1);
+    }
 }
-
--- Lab_1/src/main/java/ru/relz/javacore2017/GetNetAddress.java
+++ Lab_1/src/main/java/ru/relz/javacore2017/GetNetAddress.java
@@ -1,31 +1,38 @@
 package ru.relz.javacore2017;
 
 class GetNetAddress {
-	private static int bytesBlockCount = 4;
+    private static int bytesBlockCount = 4;
 
-	public static void main(String[] args) {
-		if (args.length < 2) {
-			printlnError("usage: GetNetAddress <ip address> <network mask>");
-		}
-		byte[] ipAddress = new byte[bytesBlockCount];
-		getArgument(args[0], ipAddress);
-		byte[] networkMask = new byte[bytesBlockCount];
-		getArgument(args[1], networkMask);
-		for (int i = 0; i < bytesBlockCount; ++i) {
-			System.out.print((ipAddress[i] & networkMask[i] & 0xFF) + ".");
-		}
-	}
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            printlnError("usage: GetNetAddress <ip address> <network mask>");
+        }
+        byte[] ipAddress = new byte[bytesBlockCount];
+        getArgument(args[0], ipAddress);
+        byte[] networkMask = new byte[bytesBlockCount];
+        getArgument(args[1], networkMask);
+        for (int i = 0; i < bytesBlockCount; ++i) {
+            System.out.print((ipAddress[i] & networkMask[i] & 0xFF));
+            if (i != bytesBlockCount - 1) {
+                System.out.print(".");
+            }
+        }
+        System.out.println();
+    }
 
-	private static void printlnError(String msg) {
-		System.out.println(String.format("Error: %s", msg));
-		System.exit(1);
-	}
+    private static void printlnError(String msg) {
+        System.out.println(String.format("Error: %s", msg));
+        System.exit(1);
+    }
 
-	private static void getArgument(String arg, byte[] result) {
-		String[] bytesStrs = arg.split("[.]");
-		for (int i = 0; i < bytesStrs.length; ++i) {
-			result[i] = (byte) Integer.parseInt(bytesStrs[i]);
-		}
-	}
+    private static void getArgument(String arg, byte[] result) {
+        String[] bytesStrs = arg.split("[.]");
+        for (int i = 0; i < bytesStrs.length; ++i) {
+            try {
+                result[i] = (byte) Integer.parseInt(bytesStrs[i]);
+            } catch (NumberFormatException ex) {
+                printlnError("Invalid number: " + bytesStrs[i]);
+            }
+        }
+    }
 }
-
