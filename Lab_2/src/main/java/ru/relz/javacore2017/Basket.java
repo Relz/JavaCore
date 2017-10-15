ba4fc84ce52502c96c656821614c2f23418d9d8d
Starovoytov Alexey <relz0071@gmail.com>
2017-10-15 13:26:23 +0000

    Исправил отступы, добавил модификатор final к списку продуктов в корзине

--- Lab_2/src/main/java/ru/relz/javacore2017/Basket.java
+++ Lab_2/src/main/java/ru/relz/javacore2017/Basket.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Basket {
-	private List<IProduct> _products = new ArrayList<>();
+	private final List<IProduct> _products = new ArrayList<>();
 
-/**
- * Adds a Product object to the Basket object back.
- * */
+	/**
+	 * Adds a Product object to the Basket object back.
+	 */
 	void add(IProduct product) {
 		_products.add(product);
 	}
 
-/**
- * Returns a Product object located in the Basket object beginning, removing it from Basket object.
- *
- * @return the product
- * */
+	/**
+	 * Returns a Product object located in the Basket object beginning, removing it from Basket object.
+	 *
+	 * @return the product
+	 */
 	IProduct get() {
 		return _products.remove(0);
 	}
 
-/**
- * Returns {@code true} if Basket object contains no elements.
- *
- * @return {@code true} if Basket object contains no elements
- */
+	/**
+	 * Returns {@code true} if Basket object contains no elements.
+	 *
+	 * @return {@code true} if Basket object contains no elements
+	 */
 	boolean isEmpty() {
 		return _products.isEmpty();
 	}
