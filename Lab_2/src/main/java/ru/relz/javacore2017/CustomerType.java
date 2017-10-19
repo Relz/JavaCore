eb729b986974e203024f14e89b6f02dea60332f7
Starovoytov Alexey <relz0071@gmail.com>
2017-10-19 21:01:01 +0000

    Добавлен метод получения строки из типа покупателя и метод получения случайного покупателя

--- Lab_2/src/main/java/ru/relz/javacore2017/CustomerType.java
+++ Lab_2/src/main/java/ru/relz/javacore2017/CustomerType.java
@@ -3,5 +3,23 @@ package main.java.ru.relz.javacore2017;
 enum CustomerType {
 	Child,
 	Adult,
-	Retired
+	Retired;
+
+	@Override
+	public String toString() {
+		switch (this) {
+			case Child:
+				return "Ребёнок";
+			case Adult:
+				return "Взрослый";
+			case Retired:
+				return "Пенсионер";
+			default:
+				return "Неизвестный";
+		}
+	}
+
+	static CustomerType getRandom() {
+		return CustomerType.values()[(int) (Math.random() * (CustomerType.values().length - 1))];
+	}
 }
