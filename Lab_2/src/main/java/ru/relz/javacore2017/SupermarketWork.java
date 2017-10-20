676b080c6cb12d136319f27dd8c6b3e2b5883e99
Starovoytov Alexey <relz0071@gmail.com>
2017-10-20 12:41:34 +0000

    Добавлена логика случайной работы магазина Покупатели могут: -попытаться выйти из магазина с неоплаченными продуктами в корзине; -вернуть товар из корзины; -выйти без покупок.

--- Lab_2/src/main/java/ru/relz/javacore2017/SupermarketWork.java
+++ Lab_2/src/main/java/ru/relz/javacore2017/SupermarketWork.java
@@ -1,5 +1,7 @@
 package main.java.ru.relz.javacore2017;
 
+import java.util.Iterator;
+
 class SupermarketWork implements SupermarketWorkInterface {
 
 	@Override
@@ -7,16 +9,46 @@ class SupermarketWork implements SupermarketWorkInterface {
 		for (int i = 0; i < getRandomNumber(0, 5); ++i) {
 			supermarket.addCustomer(new Customer(CustomerType.getRandom()));
 		}
-		supermarket.getCustomers().forEach((Customer customer) -> {
+		supermarket.forEachCustomer((Iterator<Customer> customerIterator) -> {
+			Customer customer = customerIterator.next();
+
 			for (int i = 0; i < getRandomNumber(0, 12); ++i) {
 				int randomProductIndex = getRandomNumber(0, supermarket.getProducts().size() - 1);
 				Product randomProduct = supermarket.getProducts().get(randomProductIndex);
 				int productId = randomProduct.getId();
 				int productMaxAmount = randomProduct.getAmount();
 				int productAmount = getRandomProductAmount(randomProduct, productMaxAmount);
-				Product wantedProduct = supermarket.getProduct(customer.getType(), productId, productAmount);
+				Product wantedProduct = supermarket.getProduct(customer, productId, productAmount);
 				if (wantedProduct != null) {
 					customer.getBacket().add(wantedProduct);
+				}
+			}
+			if (getRandomNumber(0, 1) == 0) {
+				if (getRandomNumber(0, 1) == 0) {
+					int randomNumber = getRandomNumber(0, 2);
+					if (randomNumber == 0) {
+						// Try to get out without returning back products
+						supermarket.removeCustomer(customerIterator, customer);
+					} else if (randomNumber == 1) {
+						// Return back products and get out
+						customer.getBacket().forEachProduct((Iterator<Product> productIterator) -> {
+							Product product = productIterator.next();
+							customer.getBacket().get(productIterator);
+							supermarket.returnProductBack(product, customer);
+						});
+						supermarket.removeCustomer(customerIterator, customer);
+					} else {
+						// May be return back some products
+						customer.getBacket().forEachProduct((Iterator<Product> productIterator) -> {
+							Product product = productIterator.next();
+							if (getRandomNumber(0, 4) == 0) {
+								customer.getBacket().get(productIterator);
+								supermarket.returnProductBack(product, customer);
+							}
+						});
+					}
+				} else {
+
 				}
 			}
 		});
@@ -25,6 +57,7 @@ class SupermarketWork implements SupermarketWorkInterface {
 
 	@Override
 	public void onFinished() {
+
 	}
 
 	private static int getRandomNumber(int from, int to) {
