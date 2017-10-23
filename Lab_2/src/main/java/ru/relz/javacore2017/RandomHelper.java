145ff4b95fbbdcdf18d7e68b053aefda63541afb
Starovoytov Alexey <relz0071@gmail.com>
2017-10-23 22:54:05 +0000

    Функции для случайных значений вынесены в RandomHelper

---
+++ Lab_2/src/main/java/ru/relz/javacore2017/RandomHelper.java
@@ -0,0 +1,22 @@
+package main.java.ru.relz.javacore2017;
+
+import java.util.List;
+
+class RandomHelper {
+	static int getRandomNumber(int from, int to) {
+		return (int) (Math.round(Math.random() * (to - from)) + from);
+	}
+	static int getRandomProductAmount(Product product, int productMaxAmount) {
+		switch (product.getType()) {
+			case Packed:
+				return getRandomNumber(1, 10);
+			case Bulk:
+				return getRandomNumber(100, 10000);
+			default:
+				return 1;
+		}
+	}
+	static PaymentMethod getRandomPaymentMethod(List<PaymentMethod> paymentMethods) {
+		return paymentMethods.get(getRandomNumber(0, paymentMethods.size() - 1));
+	}
+}
