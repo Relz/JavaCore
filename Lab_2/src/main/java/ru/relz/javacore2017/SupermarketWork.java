72833997ea7398221c6a498af161a29681a66046
Starovoytov Alexey <relz0071@gmail.com>
2017-10-23 23:03:34 +0000

    Расширена логика работы супермакрета Покупатели могут занять очередь на кассу, кассы обрабатывают очередь

--- Lab_2/src/main/java/ru/relz/javacore2017/SupermarketWork.java
+++ Lab_2/src/main/java/ru/relz/javacore2017/SupermarketWork.java
@@ -2,56 +2,62 @@ package main.java.ru.relz.javacore2017;
 
 import java.util.Iterator;
 
+import static main.java.ru.relz.javacore2017.RandomHelper.getRandomNumber;
+import static main.java.ru.relz.javacore2017.RandomHelper.getRandomProductAmount;
+
 class SupermarketWork implements SupermarketWorkInterface {
 
 	@Override
 	public void onEachTimeUnit(Supermarket supermarket) {
-		for (int i = 0; i < getRandomNumber(0, 5); ++i) {
-			supermarket.addCustomer(new Customer(CustomerType.getRandom()));
+		for (int i = 0; i < getRandomNumber(0, Supermarket.timeUnitMinutes); ++i) {
+			supermarket.addCustomer(new Customer(CustomerType.getRandom(), getRandomNumber(0, 1000), getRandomNumber(0, 10000), getRandomNumber(0, 1000)));
 		}
 		supermarket.forEachCustomer((Iterator<Customer> customerIterator) -> {
 			Customer customer = customerIterator.next();
 
-			for (int i = 0; i < getRandomNumber(0, 12); ++i) {
+			int wantedProductCount = getRandomNumber(0, 6);
+			for (int i = 0; i < wantedProductCount; ++i) {
 				int randomProductIndex = getRandomNumber(0, supermarket.getProducts().size() - 1);
 				Product randomProduct = supermarket.getProducts().get(randomProductIndex);
 				int productId = randomProduct.getId();
 				int productMaxAmount = randomProduct.getAmount();
-				int productAmount = getRandomProductAmount(randomProduct, productMaxAmount);
+				int productAmount = getRandomProductAmount(randomProduct, productMaxAmount / 5);
 				Product wantedProduct = supermarket.getProduct(customer, productId, productAmount);
 				if (wantedProduct != null) {
 					customer.getBacket().add(wantedProduct);
 				}
 			}
 			if (getRandomNumber(0, 1) == 0) {
-				if (getRandomNumber(0, 1) == 0) {
-					int randomNumber = getRandomNumber(0, 2);
-					if (randomNumber == 0) {
-						// Try to get out without returning back products
-						supermarket.removeCustomer(customerIterator, customer);
-					} else if (randomNumber == 1) {
-						// Return back products and get out
-						customer.getBacket().forEachProduct((Iterator<Product> productIterator) -> {
-							Product product = productIterator.next();
+				int randomNumber = getRandomNumber(0, 6);
+				if (randomNumber == 0) {
+					// Try to get out without returning back products
+					supermarket.removeCustomer(customerIterator, customer);
+				} else if (randomNumber == 1) {
+					// Return back products and get out
+					customer.getBacket().forEachProduct((Iterator<Product> productIterator) -> {
+						Product product = productIterator.next();
+						customer.getBacket().get(productIterator);
+						supermarket.returnProductBack(product, customer);
+					});
+					supermarket.removeCustomer(customerIterator, customer);
+				} else {
+					// May be return back some products
+					customer.getBacket().forEachProduct((Iterator<Product> productIterator) -> {
+						Product product = productIterator.next();
+						if (getRandomNumber(0, 4) == 0) {
 							customer.getBacket().get(productIterator);
 							supermarket.returnProductBack(product, customer);
-						});
-						supermarket.removeCustomer(customerIterator, customer);
-					} else {
-						// May be return back some products
-						customer.getBacket().forEachProduct((Iterator<Product> productIterator) -> {
-							Product product = productIterator.next();
-							if (getRandomNumber(0, 4) == 0) {
-								customer.getBacket().get(productIterator);
-								supermarket.returnProductBack(product, customer);
-							}
-						});
-					}
-				} else {
-
+						}
+					});
 				}
 			}
+			if (!customer.isInQueue() && !customer.getBacket().isEmpty()) {
+				// Join to cash desk
+				CashDesk bestCashDesk = supermarket.getBestCashDesk();
+				bestCashDesk.addCustomerToQueue(customer);
+			}
 		});
+		supermarket.processCashDesks();
 	}
 
 	@Override
@@ -65,20 +71,5 @@ class SupermarketWork implements SupermarketWorkInterface {
 			});
 			supermarket.removeCustomer(customerIterator, customer);
 		});
-	}
-
-	private static int getRandomNumber(int from, int to) {
-		return (int) (Math.random() * to) + from;
-	}
-
-	private static int getRandomProductAmount(Product product, int productMaxAmount) {
-		switch (product.getType()) {
-			case Packed:
-				return getRandomNumber(1, 10);
-			case Bulk:
-				return getRandomNumber(100, 10000);
-			default:
-				return 1;
-		}
 	}
 }
