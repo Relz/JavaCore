646796a7d38a5d5485f4d42e08c3ba1122ed0570
Starovoytov Alexey <relz0071@gmail.com>
2017-10-23 22:58:56 +0000

    Покупателю добавлены разные возможности оплаты и метод получения случайного способа оплаты в зависимости от цены вопроса

--- Lab_2/src/main/java/ru/relz/javacore2017/Customer.java
+++ Lab_2/src/main/java/ru/relz/javacore2017/Customer.java
@@ -1,8 +1,16 @@
 package main.java.ru.relz.javacore2017;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static main.java.ru.relz.javacore2017.RandomHelper.getRandomPaymentMethod;
+
 class Customer {
-	Customer(CustomerType type) {
+	Customer(CustomerType type, double cash, double cardCash, double bonusCount) {
 		_type = type;
+		_cash = cash;
+		_cardCash = cardCash;
+		_bonusCount = bonusCount;
 	}
 
 	private final CustomerType _type;
@@ -10,8 +18,64 @@ class Customer {
 		return _type;
 	}
 
+	private double _cash = 0.0;
+	double getCash() {
+		return _cash;
+	}
+
+	void setCash(double value) {
+		_cash = value;
+	}
+
+	private double _cardCash = 0.0;
+	double getCardCash() {
+		return _cardCash;
+	}
+
+	void setCardCash(double value) {
+		_cardCash = value;
+	}
+
+	private Double _bonusCount = 0.0;
+	Double getBonusCount() {
+		return _bonusCount;
+	}
+
+	void setBonusCount(double value) {
+		_bonusCount = value;
+	}
+
 	private final Basket _basket = new Basket();
 	Basket getBacket() {
 		return _basket;
+	}
+
+	private boolean _inQueue = false;
+	boolean isInQueue() {
+		return _inQueue;
+	}
+
+	void setInQueue(boolean value) {
+		_inQueue = value;
+	}
+
+	PaymentMethod getDesiredPaymentMethod(double totalPaymentAmount) {
+		List<PaymentMethod> availablePaymentMethods = new ArrayList<>();
+		if (_cash > totalPaymentAmount) {
+			availablePaymentMethods.add(PaymentMethod.Cash);
+		}
+		if (_cardCash > totalPaymentAmount) {
+			availablePaymentMethods.add(PaymentMethod.Card);
+		}
+		if (_bonusCount > totalPaymentAmount) {
+			availablePaymentMethods.add(PaymentMethod.Bonuses);
+			availablePaymentMethods.add(PaymentMethod.Bonuses);
+			availablePaymentMethods.add(PaymentMethod.Bonuses);
+		}
+		if (availablePaymentMethods.isEmpty()) {
+			return PaymentMethod.None;
+		}
+
+		return getRandomPaymentMethod(availablePaymentMethods);
 	}
 }
