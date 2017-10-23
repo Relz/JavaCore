7712bd08fd512cf6fc71fc4db41248c1f60b64ad
Starovoytov Alexey <relz0071@gmail.com>
2017-10-23 23:02:32 +0000

    Расширение супермаркета имеет возможность добавлять кассы, добавлен метод получения наиболее выгодной кассы, добавлен метод обработки работы касс

--- Lab_2/src/main/java/ru/relz/javacore2017/Supermarket.java
+++ Lab_2/src/main/java/ru/relz/javacore2017/Supermarket.java
@@ -1,12 +1,16 @@
 package main.java.ru.relz.javacore2017;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.function.Consumer;
 
+import static main.java.ru.relz.javacore2017.RandomHelper.getRandomNumber;
+
 class Supermarket {
-	private static final int timeUnitMinutes = 5;
+	static final int timeUnitMinutes = 5;
+
 	private static final int _secondsInMinute = 60;
 	private static final int _millisecondsInSecond = 1000;
 
@@ -14,8 +18,9 @@ class Supermarket {
 	private final DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 	private final Date startDate = new Date();
 	private final HashMap<Customer, Integer> _customersToIds = new HashMap<>();
-	private int _customerId = 0;
+	private final List<CashDesk> _cashDesks = new ArrayList<>();
 
+	private int _customerId = 0;
 	private int _workingTimeLeft = 0;
 
 	Supermarket() {
@@ -35,6 +40,24 @@ class Supermarket {
 	private List<Product> _products = new ArrayList<>();
 	List<Product> getProducts() {
 		return _products;
+	}
+
+	private HashMap<CustomerType, Integer> _discountPercentages = new HashMap<CustomerType, Integer>() {{
+		put(CustomerType.Child, 0);
+		put(CustomerType.Adult, 0);
+		put(CustomerType.Retired, 20);
+	}};
+	HashMap<CustomerType, Integer> getDiscountPercentages() {
+		return _discountPercentages;
+	}
+	/**
+	 * Adds cash desk to supermarket with specified name
+	 *
+	 * @param name name for cash desk
+	 * */
+	void addCashDesk(String name) {
+		CashDesk cashDesk = new CashDesk(this, name);
+		_cashDesks.add(cashDesk);
 	}
 
 	/**
@@ -94,7 +117,11 @@ class Supermarket {
 			return;
 		}
 		System.out.println(customerName + " вышел из магазина");
-		customerIterator.remove();
+		if (customerIterator == null) {
+			_customers.remove(customer);
+		} else {
+			customerIterator.remove();
+		}
 	}
 
 	/**
@@ -107,19 +134,44 @@ class Supermarket {
 
 	/**
 	 * Performs supermarket work cycle until end of working time.
-	 * Call {@code onEachTimeUnit} method each unit of time.
-	 * Call {@code onFinished} method on working time ends.
+	 * Calls {@code onEachTimeUnit} method each unit of time.
+	 * Calls {@code onFinished} method on working time ends.
 	 */
-	void work(SupermarketWorkInterface supermarketWorkInterface) {
+	void work(SupermarketWorkInterface supermarketWorkInterface) throws IOException {
 		System.out.println("Магазин начал свою работу");
 		while (_workingTimeMinutes > 0) {
 			System.out.println("Сейчас " + dateFormat.format(startDate.getTime() + _workingTimeLeft * _secondsInMinute * _millisecondsInSecond));
 			supermarketWorkInterface.onEachTimeUnit(this);
 			_workingTimeMinutes -= timeUnitMinutes;
 			_workingTimeLeft += timeUnitMinutes;
+			System.in.read();
 		}
 		System.out.println("Кассы завершили свою работу, магазин готовится к закрытию");
 		supermarketWorkInterface.onFinished(this);
+	}
+
+	CashDesk getBestCashDesk() {
+		CashDesk bestCashDesk = _cashDesks.get(0);
+		for (CashDesk cashDesk : _cashDesks) {
+			if (cashDesk.isOpened() && cashDesk.getQueueSize() < bestCashDesk.getQueueSize()) {
+				bestCashDesk = cashDesk;
+			}
+		}
+
+		return bestCashDesk;
+	}
+
+	void processCashDesks() {
+		_cashDesks.forEach((CashDesk cashDesk) -> {
+			cashDesk.processQueue(Supermarket.timeUnitMinutes);
+			if (getRandomNumber(0, 10) == 0) {
+				if (cashDesk.isOpened()) {
+					cashDesk.close();
+				} else {
+					cashDesk.open();
+				}
+			}
+		});
 	}
 
 	/**
@@ -127,7 +179,7 @@ class Supermarket {
 	 *
 	 * @return customer type and number in string typedef
 	 * */
-	private String getCustomerName(Customer customer) {
+	String getCustomerName(Customer customer) {
 		return customer.getType().toString() + " " + _customersToIds.get(customer);
 	}
 }
